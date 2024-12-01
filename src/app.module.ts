7a07b358fbc1b7c292b86d710be925226c31a8a4
Levi Hutchins <levihutchins0@gmail.com>
2024-12-01 21:09:09 +1100

    revised CI pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './public/default/app.controller';
+import { AppService } from './public/default/app.service';
 
 @Module({
   imports: [],
